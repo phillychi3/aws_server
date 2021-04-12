976160cec7683cae89b2d69ce196464c84b88526
phillychi3 <55632143+phillychi3@users.noreply.github.com>
2021-04-13 00:28:39 +0800

    030

--- neko_client.py
+++ neko_client.py
@@ -11,7 +11,8 @@ class use_neko:
         self.control_q2=0
         self.control_q3=False
         self.control_code=0
-
+        self.status_code=False
+        self.server_count=0
     def control(self):
         q1=input("請輸入連結網址(link)")
         q2=input("請輸入黃金潮(數字)")
@@ -34,64 +35,84 @@ class use_neko:
                 lv.lvgetproxy()
             time.sleep(1)
             spinner.ok("✅ ")
-
-
-
-
-
-
+            time_start = time.time()            
+            self.neko_count(time_start)
+            
+            
 
     def neko_count(self,time_start):
         while True:
             time_end = time.time()
             losttime=time_end-time_start
             lostminute=int(losttime/60)
-            if lostminute<=5:
+            if lostminute<=5 and self.status_code==False :
+                self.start(1)
+            elif lostminute>=5 and lostminute<=10 and self.status_code==False:
+                self.control_code=1
+                self.start(2)
                 
-            
-
-
     def start(self,control):
-        print("start")
-        if control==1:            
+        print(f"start:{control}")        
+        time.sleep(1)
+        if control==1:
+            self.control_code=0
             threading.Thread(target=self.server01,  args=()).start()
+        if control==2:
+            self.control_code=0
+            threading.Thread(target=self.server02,  args=()).start()
+        if control==3:
+            self.control_code=0
+            threading.Thread(target=self.server03,  args=()).start()
+        if control==4:
+            self.control_code=0
+            threading.Thread(target=self.server04,  args=()).start()
 
     def server01(self):
+        print("start server01")
         while True:
             r = requests.get(self.link)
-            rd=random.randint(0.1,1)
+            rt=random.randint(1,2)
+            self.status_code=True
             if r.status_code!=200:
                 print("失去訂單")
             if self.control_code==1:
+                self.status_code=False
+                print("break server01")
                 break
-            time.sleep(rd)
+            time.sleep(rt)
 
     def server02(self):
+        print("start server02")
         while True:
             r = requests.get(self.link)
-            rd=random.randint(0.01,0.1)
+            self.status_code=True
             if r.status_code!=200:
                 print("失去訂單")
             if self.control_code==1:
+                self.status_code=False
+                print("break server02")
                 break
-            time.sleep(rd)
+            time.sleep(1)
 
     def server03(self):
         while True:
-            r = requests.get(self.link)            
+            r = requests.get(self.link)
+            self.status_code=True
             if r.status_code!=200:
                 print("失去訂單")
             if self.control_code==1:
+                self.status_code=False
                 break
             
 
     def server04(self):
         while True:
             r = requests.get(self.link)
-            
+            self.status_code=True
             if r.status_code!=200:
                 print("失去訂單")
             if self.control_code==1:
+                self.status_code=False
                 break
             
             
